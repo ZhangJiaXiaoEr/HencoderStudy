bd4c497f68de1e4af238d458e1829c948129a301
ZhangJiaXiaoEr <1062826385@qq.com>
2022-04-05 12:38:59 +0800

    添加一个各个手指互不干扰的画线的View

---
+++ customview/src/main/java/com/zjx/customview/L24/MultiTouchView3.kt
@@ -0,0 +1,58 @@
+package com.zjx.customview.L24
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.SparseArray
+import android.view.MotionEvent
+import android.view.View
+import com.zjx.customview.dp
+
+class MultiTouchView3(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth = 2f.dp
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+    }
+    private val paths = SparseArray<Path>()
+
+    override fun onDraw(canvas: Canvas) {
+        for (i in 0 until paths.size()) {
+            val path = paths.valueAt(i)
+            canvas.drawPath(path, paint)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when(event.actionMasked) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN->{
+                val pointerId = event.getPointerId(event.actionIndex)
+                val path = Path()
+                path.moveTo(event.getX(event.actionIndex), event.getY(event.actionIndex))
+                paths.append(pointerId, path)
+                invalidate()
+            }
+            MotionEvent.ACTION_MOVE->{
+                for (i in 0 until paths.size()) {
+                    val pointerId = event.getPointerId(i)
+                    val path = paths.get(pointerId)
+                    path.lineTo(event.getX(i), event.getY(i))
+                    invalidate()
+                }
+            }
+            MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_UP->{
+                val pointerId = event.getPointerId(event.actionIndex)
+                val path = paths.get(pointerId)
+                path.reset()
+                paths.remove(pointerId)
+                invalidate()
+            }
+        }
+        return true
+    }
+
+}
