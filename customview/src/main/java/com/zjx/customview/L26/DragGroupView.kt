3500935a7f061576a5c38ad52501c0ea718d773f
ZhangJiaXiaoEr <1062826385@qq.com>
2022-04-06 23:20:20 +0800

    添加拖动的GridView

--- customview/src/main/java/com/zjx/customview/L26/DragGroupView.kt
+++ customview/src/main/java/com/zjx/customview/L26/DragGroupView.kt
@@ -2,12 +2,19 @@ package com.zjx.customview.L26
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.util.SparseArray
+import android.view.DragEvent
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.children
 
 class DragGroupView(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
     private val columns = 2
     private val rows = 3
+    private var dragView: View? = null
+    private var orderdChildren: MutableList<View> = ArrayList()
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val specWidth = MeasureSpec.getSize(widthMeasureSpec)
         val specHeight = MeasureSpec.getSize(heightMeasureSpec)
@@ -25,8 +32,84 @@ class DragGroupView(context: Context?, attrs: AttributeSet?) : ViewGroup(context
         val childHeight = height/rows
         for ((index, child) in children.withIndex()) {
             childLeft = index%columns * childWidth
-            childTop = index/rows * childHeight
-            child.layout(childLeft, childTop, childLeft+childWidth, childTop+childHeight)
+            childTop = index/columns * childHeight
+            child.layout(0, 0, childWidth, childHeight)
+            child.translationX = childLeft.toFloat()
+            child.translationY = childTop.toFloat()
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        for ((index, child) in children.withIndex()){
+            child.setTag(index)
+            orderdChildren.add(child)
+            child.setOnLongClickListener {
+                dragView = it
+                it.startDrag(null, DragShadowBuilder(it), it, 0)
+                false
+            }
+
+            child.setOnDragListener(MyOnDragListener())
+        }
+    }
+
+    inner class MyOnDragListener: OnDragListener{
+        override fun onDrag(view: View, event: DragEvent): Boolean {
+            when(event.action) {
+                //拖动开始
+                DragEvent.ACTION_DRAG_STARTED->{
+                    if (view === event.localState) {
+                        view.visibility = View.INVISIBLE
+                    }
+                }
+                //表示有drag的view进入到当前view的区域
+                DragEvent.ACTION_DRAG_ENTERED->{
+                    if (view !== event.localState) {
+                        sort(view)
+                    }
+                }
+                //表示drag的view在当前的view的区域中，改变了它的位置
+                DragEvent.ACTION_DRAG_LOCATION->{}
+                //表示drag的view离开了当前的view的区域
+                DragEvent.ACTION_DRAG_EXITED->{}
+                //真正的drop操作，就是你的drag动作松开手指
+                DragEvent.ACTION_DROP->{}
+                //drag和drop操作都结束。
+                DragEvent.ACTION_DRAG_ENDED->{
+                    if (view === event.localState) {
+                        view.visibility = View.VISIBLE
+                    }
+                }
+            }
+            return true
+        }
+    }
+
+    private fun sort(view: View) {
+        var targetIndex = 0
+        var dragIndex = 0
+        for ((index, child) in orderdChildren.withIndex()) {
+            if (child === view) {
+                targetIndex = index
+            }else if (view === dragView) {
+                dragIndex = index
+            }
+        }
+        orderdChildren.removeAt(dragIndex)
+        orderdChildren.add(targetIndex, dragView!!)
+        var childLeft = 0
+        var childTop = 0
+        val childWidth = width/columns
+        val childHeight = height/rows
+        for ((index, child) in orderdChildren.withIndex()) {
+            childLeft = index%columns * childWidth
+            childTop = index/columns * childHeight
+            Log.e("DragGroup", "dragIndex=$dragIndex, targetIndex=$targetIndex, childLeft=$childLeft, childeTop=$childTop, tag=${child.tag}")
+            child.animate()
+                .setDuration(150)
+                .translationX(childLeft.toFloat())
+                .translationY(childTop.toFloat())
         }
     }
 }
